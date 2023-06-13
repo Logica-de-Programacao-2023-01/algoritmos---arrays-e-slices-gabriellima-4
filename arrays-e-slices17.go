c884ba4501295fa0c9c688d0b700f4df4ef695ce
unknown <gabrilima2004@sempreceub.com>
2023-06-13 17:45:36 -0300

    solução AS17

---
+++ arrays-e-slices17.go
@@ -0,0 +1,11 @@
+//Crie um Array de inteiros com 10 elementos. Calcule e imprima a soma dos elementos nas posições pares do Array.
+package main
+
+import "fmt"
+
+func main() {
+	array := [10]int{1,2,3,4,5,6,7,8,9,10}
+
+	soma := array[0]+array[2]+array[4]+array[6]+array[8]
+	fmt.Println(soma)
+}
